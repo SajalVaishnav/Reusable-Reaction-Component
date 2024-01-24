bc31c69eb01134dd98654acec19051132d4ce739
SajalVaishnav <76965917+SajalVaishnav@users.noreply.github.com>
2024-01-24 10:56:19 +0800

    creating the reaction component now

--- reaction-component/src/app/components/post/BottomRibbon.tsx
+++ reaction-component/src/app/components/post/BottomRibbon.tsx
@@ -1,23 +1,58 @@
-// components/BottomRibbon.tsx
 import React from 'react';
+import Image from 'next/image'; // Import Image from Next.js
 import UsernameAndPostedTime from './bottom_ribbon/UsernameAndPostedTime';
+import CommentIcon from '../../../../public/CommentIcon.svg';
+
 interface BottomRibbonProps {
   username: string;
-  createdAt: Date; // Assuming createdAt is a string, adjust accordingly
+  createdAt: Date; 
 }
 
 const BottomRibbon: React.FC<BottomRibbonProps> = ({ username, createdAt }) => {
-  const bottomRibbonStyles: React.CSSProperties = {
+  const bottomRibbonContainerStyles: React.CSSProperties = {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     alignSelf: 'stretch',
-    fontFeatureSettings: 'ss11 on, cv09 on, liga off, calt off'
+    fontFeatureSettings: 'ss11 on, cv09 on, liga off, calt off',
+  };
+
+  const reactionAndCommentsContainerStyles: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    gap: '20px',
+    flex: '1 0 0',
+  };
+
+  const commentContainerStyles: React.CSSProperties = {
+    gap: '2px', 
+    display: 'flex', 
+    alignItems: 'center', 
+  };
+
+  const commentTypography: React.CSSProperties = {
+    color: 'var(--Dark-Grey, #494E5B)',
+    fontFamily: 'Inter',
+    fontSize: '14px',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 'normal',
+    letterSpacing: '-0.42px',
   };
 
   return (
-    <div style={bottomRibbonStyles}>
+    <div style={bottomRibbonContainerStyles}>
       <UsernameAndPostedTime username={username} createdAt={createdAt} />
+      <span style={reactionAndCommentsContainerStyles}>
+        <span style={commentContainerStyles}>
+          <span>
+            <Image src={CommentIcon} alt="Comment Icon" width={24} height={24} />
+          </span>
+          <span style={commentTypography}>42 comments</span>
+        </span>
+        {/* <ReactionComponent /> */}
+      </span>
     </div>
   );
 };
--- reaction-component/src/app/page.tsx
+++ reaction-component/src/app/page.tsx
@@ -1,46 +1,25 @@
-// pages/index.tsx
 import React from 'react';
 import Post from './components/Post';
-import { getAllPosts, getReactions } from '@/prisma/db_utils';
+import { getAllPosts } from '@/prisma/db_utils';
 import { PrismaClient } from '@prisma/client';
-
-interface HomeProps {
-  posts: Array<{
-    id: number;
-    title: string;
-    content: string;
-    author: string;
-    createdTime: string;
-  }>;
-}
 
 export default async function Home() {
   const prisma = new PrismaClient();
   const posts = await getAllPosts(prisma);
-  const reactions = await getReactions(posts[0].id, prisma);
 
   return (
-    <div>
-      <h1>Posts</h1>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          content={post.content}
-          author={post.user.username}
-          createdTime={post.createdAt}
-        />
-      ))}
-
-      {/* Display Reactions for the first post */}
-      <h2>Reactions</h2>
-      <ul>
-        {reactions.map((reaction, index) => (
-          <li key={index}>
-            {reaction.user.username} {reaction.reactionEmoji.typeDetails}
-          </li>
+    <div style={{ paddingTop: '20px' }}>
+      <div style={{ margin: '0 auto', maxWidth: '600px' }}>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            content={post.content}
+            author={post.user.username}
+            createdTime={post.createdAt}
+          />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
